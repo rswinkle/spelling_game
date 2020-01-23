b4ac08ce699d35b6631bdd345a3a13c6031dbcbb
Robert Winkler <rob121618@gmail.com>
2020-01-22 17:24:09 -0700

    Add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+-- A solution contains projects, and defines the available configurations
+solution "Spelling_Game"
+	configurations { "Debug", "Release" }
+	location "build"
+
+	-- A project defines one build target
+	project "spelling_game"
+		location "build"
+		kind "ConsoleApp"
+		language "C"
+		files
+		{
+			"main.c",
+			"spelling_game.h",
+			"cvector.h",
+			"linenoise.c",
+			"linenoise.h",
+			"c_utils.c",
+			"c_utils.h",
+			"basic_types.h"
+		}
+--      excludes { "vector_template.*" }
+--      libdirs { }
+--      links { "cunit" } 
+		targetdir "build"
+
+		configuration "Debug"
+			defines { "DEBUG" }
+			symbols "On"
+
+		configuration "Release"
+			defines { "NDEBUG" }
+			optimize "On"
+
+		configuration { "gmake" }
+			buildoptions
+			{
+			"-std=gnu99", --for posix stuff
+			--"-pedantic-errors",
+			"-fno-strict-aliasing",
+			"-Wunused-variable",
+			"-Wreturn-type" 
+			}
